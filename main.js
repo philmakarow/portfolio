cae40140fcc3a80e30534a6f21e6eea29a2b31da
Phil Makarow <philmakarow@hotmail.com>
2023-01-09 13:07:38 -0400

    commit1

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+    $('#idpic').on('click', () => {
+         $('#pic1').toggle(500);
+    })
+    $('#waveclick').on('click', () => {
+        $('#wavelink').toggle(500);
+   })
+   $('#dasmotoclick').on('click', () => {
+    $('#dasmotolink').toggle(500);
+})
+    $('#quoteclick').on('click', () => {
+    $('#quotelink').toggle(500);
+    })
+});
